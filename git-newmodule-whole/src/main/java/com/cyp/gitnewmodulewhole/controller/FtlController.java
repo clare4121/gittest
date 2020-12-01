7fe4725cc77219abb9cd37876e767fdfbbd414b7
0113976 <chenyapeng@bonc.com.cn>
2020-12-01 10:41:07 +0800

    feat(父module的pom文件修改)

--- git-newmodule-whole/src/main/java/com/cyp/gitnewmodulewhole/controller/FtlController.java
+++ git-newmodule-whole/src/main/java/com/cyp/gitnewmodulewhole/controller/FtlController.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class FtlController {
     @RequestMapping("ftlDemo")
     public ModelAndView index(HttpServletRequest request){
-        ModelAndView modelAndView = new ModelAndView("camel/main_new");
+        ModelAndView modelAndView = new ModelAndView("demo/login");
         return modelAndView;
     }
 
