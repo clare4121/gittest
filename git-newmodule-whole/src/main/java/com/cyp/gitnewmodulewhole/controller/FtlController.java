ca81f3e780873b428847a716ed2a467916f41a95
0113976 <chenyapeng@bonc.com.cn>
2020-12-01 10:35:25 +0800

    feat(前端页面freeMaker配置)

---
+++ git-newmodule-whole/src/main/java/com/cyp/gitnewmodulewhole/controller/FtlController.java
@@ -0,0 +1,21 @@
+package com.cyp.gitnewmodulewhole.controller;
+
+import com.cyp.gitnewmodulewhole.entity.UserEntity;
+import org.apache.ibatis.io.Resources;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+@RequestMapping("/ftlIndex/")
+public class FtlController {
+    @RequestMapping("ftlDemo")
+    public ModelAndView index(HttpServletRequest request){
+        ModelAndView modelAndView = new ModelAndView("camel/main_new");
+        return modelAndView;
+    }
+
+}
